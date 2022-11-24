99eafb4989edabf6cce4411631642ec1f8d7cdb2
Tom Ecrepont <tomecrepont@gmail.com>
2022-11-24 13:14:45 +0100

    add validations on Company and RecurringEvent

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,6 @@
 class Company < ApplicationRecord
+  validates :name, format: { with: /\A[a-zA-Z-]+\z/,
+                             message: 'only allows letters and dashes' }
+
+  has_many :recurring_events
 end
--- app/models/recurring_event.rb
+++ app/models/recurring_event.rb
@@ -1,3 +1,5 @@
 class RecurringEvent < ApplicationRecord
+  validates :group_size, numericality: { greater_than: 0, less_than_or_equal_to: 10 }
+  validates :name, uniqueness: { scope: :company_id }
   belongs_to :company
 end
