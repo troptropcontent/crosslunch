e1368826f92e703427a78647c76e8192a8730fd2
Tom Ecrepont <tomecrepont@gmail.com>
2022-11-27 13:50:09 +0100

    update factories

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :event do
-    recurring_event { nil }
-    date { "2022-11-24" }
+    recurring_event
+    happens_at { '2022-11-24 12:30:00' }
   end
 end
--- spec/factories/recurring_events.rb
+++ spec/factories/recurring_events.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :recurring_event do
     name { 'MyString' }
-    week_day { 1 }
-    hour { '2022-11-24 12:55:55' }
+    weekday { :monday }
+    time { '2022-11-24 12:30:00' }
     group_size { 1 }
     company
   end
