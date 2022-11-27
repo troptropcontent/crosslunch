65fc83d1c0788e0f2aa860858b1ceed37c85d0a0
Tom Ecrepont <tomecrepont@gmail.com>
2022-11-27 13:45:28 +0100

    create employee.participates_to(event) and its spec

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,7 @@
 class Employee < ApplicationRecord
   belongs_to :company
+
+  def participates_to?(event)
+    Participation.exists?(event: event, employee_id: id)
+  end
 end
--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.participates_to?' do
+    subject { FactoryBot.create(:employee, company: company) }
+    let(:company) { FactoryBot.create(:company) }
+    let(:recurring_event) { FactoryBot.create(:recurring_event, company: company) }
+    let(:event) { FactoryBot.create(:event, recurring_event: recurring_event) }
+    describe 'when the employee is participating to the event' do
+      let!(:participation) { FactoryBot.create(:participation, event: event, employee: subject) }
+      it 'returns true' do
+        expect(subject.participates_to?(event)).to be true
+      end
+    end
+    describe 'when the employee is not participating to the event' do
+      it 'returns false' do
+        expect(subject.participates_to?(event)).to be false
+      end
+    end
+  end
 end
