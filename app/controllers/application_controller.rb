2c658d232a94c1b415f33f72f4fe7f5d9bfa4770
Tom Ecrepont <tomecrepont@gmail.com>
2022-11-27 13:48:01 +0100

    add application controller the ability to require subdomain and load things

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,36 @@
 class ApplicationController < ActionController::Base
+  helper_method :require_subdomain
+  helper_method :load_company
+  helper_method :authenticate_employee
+
+  private
+
+  def load_company
+    @company = Company.find_by(name: request.subdomain)
+  end
+
+  def require_subdomain
+    not_found unless subdomain_exist?
+  end
+
+  def subdomain_exist?
+    company = Company.find_by(name: request.subdomain)
+    !!company
+  end
+
+  def not_found
+    raise ActionController::RoutingError, '404'
+  end
+
+  def current_employee
+    Employee.find_by(id: session[:employee_id])
+  end
+
+  def logged_in?
+    !current_employee.nil?
+  end
+
+  def authenticate_employee
+    redirect_to '/login' unless logged_in?
+  end
 end
