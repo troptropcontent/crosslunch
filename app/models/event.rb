c4099d61c971acc39332c3af36a1fd7181789c38
Tom Ecrepont <tomecrepont@gmail.com>
2022-11-27 14:08:18 +0100

    create event.next and its specs

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ApplicationRecord
   belongs_to :recurring_event
+  has_many :participations
+
+  def next
+    next_date = happens_at.next_occurring(recurring_event.weekday.to_sym)
+    recurring_event.events.find_or_create_by(happens_at: next_date)
+  end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
+require 'support/shared_context/dates'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include ActiveSupport::Testing::TimeHelpers
+  include_context 'dates'
+  describe '.next' do
+    subject { event }
+    let!(:event) { FactoryBot.create(:event, recurring_event: recurring_event, happens_at: wednesday) }
+    let(:recurring_event) { FactoryBot.create(:recurring_event, weekday: :wednesday, time: wednesday) }
+    describe 'when the next event already exists' do
+      let!(:next_event) { FactoryBot.create(:event, recurring_event: recurring_event, happens_at: next_wednesday) }
+      it 'it returns the next event' do
+        expect(subject.next.happens_at).to eq(next_wednesday)
+      end
+      it 'does not create an event' do
+        expect { subject.next }.not_to change(described_class, :count)
+      end
+    end
+    describe 'when the next event does not already exist' do
+      it 'it returns the next event' do
+        expect(subject.next.happens_at).to eq(next_wednesday)
+      end
+      it 'does not create an event' do
+        expect { subject.next }.to change(described_class, :count).by(1)
+      end
+    end
+  end
 end
