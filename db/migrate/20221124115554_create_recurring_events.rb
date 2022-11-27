3ce29c30c908a6e0b2dd6680431a8e6c54b980cc
Tom Ecrepont <tomecrepont@gmail.com>
2022-11-27 13:50:25 +0100

    change columns

--- db/migrate/20221124115554_create_recurring_events.rb
+++ db/migrate/20221124115554_create_recurring_events.rb
@@ -2,8 +2,8 @@ class CreateRecurringEvents < ActiveRecord::Migration[7.0]
   def change
     create_table :recurring_events do |t|
       t.string :name, null: false
-      t.integer :week_day, null: false
-      t.time :hour, null: false
+      t.integer :weekday, null: false
+      t.time :time, null: false
       t.integer :group_size, null: false
       t.references :company, null: false, foreign_key: true
 
--- db/migrate/20221124115640_create_events.rb
+++ db/migrate/20221124115640_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration[7.0]
   def change
     create_table :events do |t|
       t.references :recurring_event, null: false, foreign_key: true
-      t.date :date, null: false
+      t.datetime :happens_at, null: false
 
       t.timestamps
     end
