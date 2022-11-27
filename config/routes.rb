d0d110b66600f2bbcf75689a04abdeac448b64c0
Tom Ecrepont <tomecrepont@gmail.com>
2022-11-27 13:51:27 +0100

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  class Subdomain
+    def self.matches?(request)
+      request.subdomain.present? && request.subdomain != 'www'
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  constraints Subdomain do
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    root 'recurring_events#show'
+    resources :participations, only: %i[create destroy]
+  end
 end
