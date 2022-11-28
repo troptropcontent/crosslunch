cd930bf117c9204cb063bca58e6d597bbc5fbe58
Tom Ecrepont <tomecrepont@gmail.com>
2022-11-28 13:48:37 +0100

    create a seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+company = Company.create(name: 'matera')
+50.times do
+  company.employees.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
+recurring_event = company.create_recurring_event!(
+  name: 'Cross Team Lunch',
+  time: Time.current.midday.change(min: 30),
+  weekday: :monday,
+  group_size: 6
+)
+event = recurring_event.next_event
+
+company.employees.sample(30).each do |employee|
+  event.participations.create(employee: employee)
+end
