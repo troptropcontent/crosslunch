b313b16075db0e96873c9a0264823a06b902676b
Tom Ecrepont <tomecrepont@gmail.com>
2022-11-28 10:27:08 +0100

    create some shared context

---
+++ spec/support/shared_context/events/5_participants.rb
@@ -0,0 +1,8 @@
+RSpec.shared_context '5 participants to the current event' do
+  5.times do |i|
+    let!("employee_#{i + 1}".to_sym) { FactoryBot.create(:employee, company: event.company) }
+    let!("participation_#{i + 1}".to_sym) do
+      FactoryBot.create(:participation, event: event, employee: send("employee_#{i + 1}".to_sym))
+    end
+  end
+end
