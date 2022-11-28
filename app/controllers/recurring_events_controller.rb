4f2a39fd9f134273860e8e11bb7a8eaec35aec3a
Tom Ecrepont <tomecrepont@gmail.com>
2022-11-28 13:49:31 +0100

    create the group_done page

--- app/controllers/recurring_events_controller.rb
+++ app/controllers/recurring_events_controller.rb
@@ -6,14 +6,12 @@ class RecurringEventsController < ApplicationController
   before_action :load_recurring_event
   include ActiveSupport::Testing::TimeHelpers
   def show
-    travel_to Time.current + 2.hours do
-      actor = RecurringEvents::FindViewAndEvent.call(employee: current_employee,
-                                                     recurring_event: @recurring_event)
-      if actor.view == :current_event
-        render_current_event_view(actor)
-      else
-        render_upcoming_event_view(actor)
-      end
+    actor = RecurringEvents::FindViewAndEvent.call(employee: current_employee,
+                                                   recurring_event: @recurring_event)
+    if actor.view == :current_event
+      render_current_event_view(actor)
+    else
+      render_upcoming_event_view(actor)
     end
   end
 
@@ -26,7 +24,8 @@ class RecurringEventsController < ApplicationController
   def render_current_event_view(actor)
     # TO DO
     @event = actor.event
-
+    @group = current_employee.group_for(@event)
+    @other_participants = @group.participations.where.not(employee: current_employee).extract_associated(:employee)
     render :current_event
   end
 
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,12 @@
 class Employee < ApplicationRecord
   belongs_to :company
+  has_many :participations
 
   def participates_to?(event)
     Participation.exists?(event: event, employee_id: id)
+  end
+
+  def group_for(event)
+    event.groups.joins(groups_participations: { participation: :employee }).find_by(employee: { id: id })
   end
 end
